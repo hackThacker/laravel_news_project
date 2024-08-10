305fdc2ae124830f36a995f95711fa970395d029
hackThacker <nirajadhiakri572@gmail.com>
2024-08-10 10:23:42 +0545

    first commit CRUD company, advertise, post, category MVC complete

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,13 @@
 <?php
 
+use App\Http\Controllers\Admin\AdvertiseController;
+use App\Http\Controllers\Admin\Categorycontroller;
+use App\Http\Controllers\Admin\CompanyController as AdminCompanyController;
+use App\Http\Controllers\Admin\post;
+use App\Http\Controllers\Admin\PostController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
+
 
 Route::get('/', function () {
     return view('welcome');
@@ -15,6 +21,11 @@ Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+
+    Route::resource('/company', AdminCompanyController::class)->names('company');
+    Route::resource('/category', Categorycontroller::class)->names('category');
+    Route::resource('/advertise', AdvertiseController::class)->names('advertise');
+    Route::resource('/post', PostController::class)->names('post');
 });
 
 require __DIR__.'/auth.php';
